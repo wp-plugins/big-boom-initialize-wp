1b575cc97267bdab4da69c23696cb18325cc7605
michaelhull <michaelhull@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-22 23:21:51 +0000

    updated plugin file version
    git-svn-id: https://plugins.svn.wordpress.org/big-boom-initialize-wp/tags/1.0.2@1204369 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- bbd-initialize.php
+++ bbd-initialize.php
@@ -4,7 +4,7 @@ Plugin Name: Big Boom Initialize WP
 Description: Initialize options and sample content for your WordPress site
 Author: Big Boom Design
 Author URI: http://www.bigboomdesign.com
-Version: 1.0.1
+Version: 1.0.2
 */
 
 /* 
